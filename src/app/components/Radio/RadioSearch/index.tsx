d0a43d77696bdd03d2391d8ee37ce1225c8bf664
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2024-10-06 23:19:47 -0300

    Update index.tsx

--- src/app/components/Radio/RadioSearch/index.tsx
+++ src/app/components/Radio/RadioSearch/index.tsx
@@ -7,22 +7,23 @@ const RadioSearch: React.FC<{
   const [query, setQuery] = useState<string>("");
 
   const handleSearch = () => {
-    setLocalQuery(query);
+    const processedQuery = query.toLowerCase().trim();
+    setLocalQuery(processedQuery);
     setQuery("");
   };
 
   return (
-    <div className="flex flex-col items-center  md:ml-0 md:mr-12 mb-4">
+    <div className="flex flex-col items-center md:ml-0 md:mr-12 mb-4">
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="Pesquisar rádios"
-        className="border p-2 mb-2 w-80 "
+        placeholder="Pesquisar por nome, país ou idioma"
+        className="border p-2 mb-2 w-80"
       />
       <button
         onClick={handleSearch}
-        className="bg-[#FF6B00] p-2 mb-2 w-80 rounded font-bold text-white transition-all duration-250 ease-in-out hover:brightness-110 hover:scale-103  "
+        className="bg-[#FF6B00] p-2 mb-2 w-80 rounded font-bold text-white transition-all duration-250 ease-in-out hover:brightness-110 hover:scale-103"
       >
         Pesquisar
       </button>
